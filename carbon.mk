3a3607a70f59b858d8855dff9bc8c9610a455626
Andros Rosa <andros2011@gmail.com>
2014-10-27 03:44:31 -0400

    n801x: Deps cleanup
    * should be using our Omni based blobs for smdk4412s
    
    Change-Id: I0f32a872a0c8097d9a260df28751cc8a898d7606

--- carbon.mk
+++ carbon.mk
@@ -18,7 +18,6 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/n801x/device.mk)
 
